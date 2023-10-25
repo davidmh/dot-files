cfb5bf2039248044c2c9a5bf1c46ac2579e88782
David Mejorado <david.mejorado@gmail.com>
2023-10-25 13:51:28 -0700

    Ensure the textwidth is set in gitcommit buffers

--- nvim/after/ftplugin/gitcommit.fnl
+++ nvim/after/ftplugin/gitcommit.fnl
@@ -1,1 +1,7 @@
-(set vim.bo.textwidth 72)
+(import-macros {: augroup} :own.macros)
+
+(fn setup-buffer []
+  (set vim.bo.textwidth 72))
+
+(augroup :git-commit-buf-enter [:BufEnter {:callback setup-buffer
+                                           :buffer 0}])
--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -1,3 +1,8 @@
 -- [nfnl] Compiled from after/ftplugin/gitcommit.fnl by https://github.com/Olical/nfnl, do not edit.
-vim.bo.textwidth = 72
+local function setup_buffer()
+  vim.bo.textwidth = 72
+  return nil
+end
+local group = vim.api.nvim_create_augroup("git-commit-buf-enter", {clear = true})
+vim.api.nvim_create_autocmd("BufEnter", {callback = setup_buffer, buffer = 0, group = group})
 return nil
