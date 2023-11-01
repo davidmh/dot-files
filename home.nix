d696aeecedbd0abd30c228fec74ef7771eeeea97
David Mejorado <david.mejorado@gmail.com>
2023-11-01 12:56:29 -0700

    Delegate nix-direnv to the project

--- home.nix
+++ home.nix
@@ -46,10 +46,7 @@ in
 
   programs.home-manager.enable = true;
 
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
+  programs.direnv.enable = true;
 
   programs.starship.enable = true;
 
