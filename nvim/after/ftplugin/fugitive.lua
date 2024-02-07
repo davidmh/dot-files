a9e684eec0ad57e022945a4f51a0095b114f51af
David Mejorado <david.mejorado@gmail.com>
2024-02-06 19:28:15 -0800

    Drop the custom fugitive resizer
    It ended up being more trouble than it was worth.

--- nvim/after/ftplugin/fugitive.lua
+++
@@ -1,6 +0,0 @@
--- [nfnl] Compiled from after/ftplugin/fugitive.fnl by https://github.com/Olical/nfnl, do not edit.
-local padding = 2
-local buffer_line_count = (vim.fn.line("$") + padding)
-local minimum_window_height = 10
-vim.api.nvim_win_set_height(0, math.max(math.min(vim.fn.winheight(0), buffer_line_count), minimum_window_height))
-return vim.keymap.set("n", "<leader>gp", "<cmd>Git! pushc<cr>", {silent = true, buffer = 0})
