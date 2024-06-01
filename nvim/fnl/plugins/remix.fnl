1b0dc875e3895259275fb5a6295726f783a7abc3
David Mejorado <david.mejorado@gmail.com>
2024-06-01 14:27:42 -0700

    Remove unused dependency

--- nvim/fnl/plugins/remix.fnl
+++ nvim/fnl/plugins/remix.fnl
@@ -1,6 +1,5 @@
 (import-macros {: use} :own.macros)
 
 (use :remix.nvim {:dir :$REMIX_HOME/.nvim
-                  :dependencies [:hrsh7th/nvim-cmp]
                   :name :remix
                   :opts {}})
--- nvim/lua/plugins/remix.lua
+++ nvim/lua/plugins/remix.lua
@@ -1,2 +1,2 @@
 -- [nfnl] Compiled from fnl/plugins/remix.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"remix.nvim", dir = "$REMIX_HOME/.nvim", dependencies = {"hrsh7th/nvim-cmp"}, name = "remix", opts = {}}
+return {"remix.nvim", dir = "$REMIX_HOME/.nvim", name = "remix", opts = {}}
