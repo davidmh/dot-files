f378dd7b28ecf7f5019ca991afac703c98e968ec
David Mejorado <594302+davidmh@users.noreply.github.com>
2024-06-04 08:22:56 -0700

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
     unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -40,7 +40,7 @@
           {
             home.username = username;
             home.homeDirectory = homeDirectory;
-            home.stateVersion = "23.11";
+            home.stateVersion = "24.05";
           }
         ];
 
