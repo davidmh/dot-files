137475a828ba326a44500fb84c847192174ecf9a
David Mejorado <david.mejorado@gmail.com>
2024-12-14 12:00:52 -0800

    Drop unused plugins

---
+++ nvim/fnl/plugins/fennel.fnl
@@ -0,0 +1,3 @@
+(import-macros {: use} :own.macros)
+
+[(use :Olical/nfnl {:ft :fennel})]
--- nvim/lua/plugins/lisp.lua
+++
@@ -1,6 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/lisp.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_()
-  vim.g.parinfer_filetypes = {"clojure", "scheme", "lisp", "fennel", "query"}
-  return nil
-end
-return {{"Olical/nfnl", ft = "fennel"}, {"clojure-vim/vim-jack-in", ft = {"clojure"}}, {"gpanders/nvim-parinfer", ft = {"clojure", "fennel", "query"}, config = _1_}}
