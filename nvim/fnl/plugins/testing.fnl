5b4806aa685a81218563afa64c2bd3e33b9170c9
David Mejorado <david.mejorado@gmail.com>
2025-03-09 13:10:39 -0700

    testing

--- nvim/fnl/plugins/testing.fnl
+++ nvim/fnl/plugins/testing.fnl
@@ -2,23 +2,27 @@
 (local {: autoload} (require :nfnl.module))
 (local {: find-root} (require :own.helpers))
 (local neotest (autoload :neotest))
+(local neotest-playwright (autoload :neotest-playwright))
 
 (fn rspec-cmd []
   (local direnv-root (find-root :.envrc))
   (local current-file (vim.fn.expand "%"))
-  (local root (vim.fn.expand (direnv-root current-file)))
+  (local root (direnv-root current-file))
   (if root
-    [:direnv :exec root :bin/rspec]
-    [:bin/rspec]))
+    [:direnv :exec (vim.fn.expand root) :bundle :exec :rspec]
+    [:bundle :exec :rspec]))
 
 (fn neotest-rspec-adapter []
   (local neotest-rspec (require :neotest-rspec))
   (neotest-rspec {:rspec_cmd rspec-cmd}))
 
 (fn config []
-  (neotest.setup {:adapters [(neotest-rspec-adapter)]
+  (neotest.setup {:log_level vim.log.levels.DEBUG
+                  :adapters [(neotest-rspec-adapter)
+                             (require :neotest-rust)
+                             (neotest-playwright.adapter {})]
                   :quickfix {:enabled true
-                             :open true}
+                             :open false}
                   :discovery {:enabled false}
                   :icons {:failed :
                           :passed :
@@ -29,9 +33,14 @@
                                            :nvim-lua/plenary.nvim
                                            :antoinemadec/FixCursorHold.nvim
                                            :nvim-treesitter/nvim-treesitter
+                                           :thenbe/neotest-playwright
+                                           :rouge8/neotest-rust
                                            :olimorris/neotest-rspec]
                             :config config
                             :keys [(use :<localleader>trn "<cmd>Neotest run<cr>" {:desc "run nearest test"})
-                                   (use :<localleader>trf #(neotest.run.run (vim.fn.expand :%)) {:desc "run test file"})
+                                   (use :<localleader>trf #(neotest.run.run (vim.fn.expand :%))
+                                                          {:desc "run test file"})
                                    (use :<localleader>ts "<cmd>Neotest summary<cr>" {:desc "open test summary"})
-                                   (use :<localleader>to "<cmd>Neotest output-panel<cr>" {:desc "open test output"})]})
+                                   (use :<localleader>to "<cmd>Neotest output-panel<cr>" {:desc "open test output"})
+                                   (use :<localleader>tjn "<cmd>Neotest jump next<cr>" {:desc "jump to next test"})
+                                   (use :<localleader>tjp "<cmd>Neotest jump prev<cr>" {:desc "jump to previous test"})]})
--- nvim/lua/plugins/testing.lua
+++ nvim/lua/plugins/testing.lua
@@ -4,14 +4,15 @@ local autoload = _local_1_["autoload"]
 local _local_2_ = require("own.helpers")
 local find_root = _local_2_["find-root"]
 local neotest = autoload("neotest")
+local neotest_playwright = autoload("neotest-playwright")
 local function rspec_cmd()
   local direnv_root = find_root(".envrc")
   local current_file = vim.fn.expand("%")
-  local root = vim.fn.expand(direnv_root(current_file))
+  local root = direnv_root(current_file)
   if root then
-    return {"direnv", "exec", root, "bin/rspec"}
+    return {"direnv", "exec", vim.fn.expand(root), "bundle", "exec", "rspec"}
   else
-    return {"bin/rspec"}
+    return {"bundle", "exec", "rspec"}
   end
 end
 local function neotest_rspec_adapter()
@@ -19,9 +20,9 @@ local function neotest_rspec_adapter()
   return neotest_rspec({rspec_cmd = rspec_cmd})
 end
 local function config()
-  return neotest.setup({adapters = {neotest_rspec_adapter()}, quickfix = {enabled = true, open = true}, discovery = {enabled = false}, icons = {failed = "\239\145\167", passed = "\239\144\174", running = "\243\176\166\150", watching = "\239\145\129"}})
+  return neotest.setup({log_level = vim.log.levels.DEBUG, adapters = {neotest_rspec_adapter(), require("neotest-rust"), neotest_playwright.adapter({})}, quickfix = {enabled = true, open = false}, discovery = {enabled = false}, icons = {failed = "\239\145\167", passed = "\239\144\174", running = "\243\176\166\150", watching = "\239\145\129"}})
 end
 local function _4_()
   return neotest.run.run(vim.fn.expand("%"))
 end
-return {"nvim-neotest/neotest", dependencies = {"nvim-neotest/nvim-nio", "nvim-lua/plenary.nvim", "antoinemadec/FixCursorHold.nvim", "nvim-treesitter/nvim-treesitter", "olimorris/neotest-rspec"}, config = config, keys = {{"<localleader>trn", "<cmd>Neotest run<cr>", desc = "run nearest test"}, {"<localleader>trf", _4_, desc = "run test file"}, {"<localleader>ts", "<cmd>Neotest summary<cr>", desc = "open test summary"}, {"<localleader>to", "<cmd>Neotest output-panel<cr>", desc = "open test output"}}}
+return {"nvim-neotest/neotest", dependencies = {"nvim-neotest/nvim-nio", "nvim-lua/plenary.nvim", "antoinemadec/FixCursorHold.nvim", "nvim-treesitter/nvim-treesitter", "thenbe/neotest-playwright", "rouge8/neotest-rust", "olimorris/neotest-rspec"}, config = config, keys = {{"<localleader>trn", "<cmd>Neotest run<cr>", desc = "run nearest test"}, {"<localleader>trf", _4_, desc = "run test file"}, {"<localleader>ts", "<cmd>Neotest summary<cr>", desc = "open test summary"}, {"<localleader>to", "<cmd>Neotest output-panel<cr>", desc = "open test output"}, {"<localleader>tjn", "<cmd>Neotest jump next<cr>", desc = "jump to next test"}, {"<localleader>tjp", "<cmd>Neotest jump prev<cr>", desc = "jump to previous test"}}}
