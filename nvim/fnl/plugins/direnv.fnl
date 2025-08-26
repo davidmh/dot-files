546beba9944f32062baa82365f0bfc99b4a96138
David Mejorado <david.mejorado@gmail.com>
2025-08-25 17:23:09 -0700

    Updated direnv api

--- nvim/fnl/plugins/direnv.fnl
+++ nvim/fnl/plugins/direnv.fnl
@@ -1,19 +1,15 @@
 (import-macros {: tx} :own.macros)
-(local {: autoload} (require :nfnl.module))
-(local direnv-opts (autoload :direnv-nvim.opts))
 
-(local git-filetypes [:git :fugitive :fugitiveblame :NeogitStatus])
-
-(fn get-basename []
-  (vim.fs.basename (vim.api.nvim_buf_get_name 0)))
+(local git-filetypes [:git :fugitive :fugitiveblame :NeogitStatus :NeogitCommitPopup])
 
 (fn get-cwd []
+  (local buf-name (vim.api.nvim_buf_get_name 0))
   (if (or (vim.tbl_contains git-filetypes vim.o.ft)
-          (vim.tbl_contains git-filetypes (get-basename)))
+          (vim.tbl_contains git-filetypes (vim.fs.basename buf-name)))
       (vim.fs.root (vim.uv.cwd) ".git")
-      (direnv-opts.buffer_setup.get_cwd)))
+      (if (vim.uv.fs_stat buf-name)
+          (vim.fs.dirname buf-name))))
 
 (tx :davidmh/direnv.nvim {:branch :custom-setup
                           :opts {:async true
-                                 :type :custom
-                                 :custom_setup {:get_cwd get-cwd}}})
+                                 :get_cwd get-cwd}})
--- nvim/lua/plugins/direnv.lua
+++ nvim/lua/plugins/direnv.lua
@@ -1,16 +1,15 @@
 -- [nfnl] fnl/plugins/direnv.fnl
-local _local_1_ = require("nfnl.module")
-local autoload = _local_1_["autoload"]
-local direnv_opts = autoload("direnv-nvim.opts")
-local git_filetypes = {"git", "fugitive", "fugitiveblame", "NeogitStatus"}
-local function get_basename()
-  return vim.fs.basename(vim.api.nvim_buf_get_name(0))
-end
+local git_filetypes = {"git", "fugitive", "fugitiveblame", "NeogitStatus", "NeogitCommitPopup"}
 local function get_cwd()
-  if (vim.tbl_contains(git_filetypes, vim.o.ft) or vim.tbl_contains(git_filetypes, get_basename())) then
+  local buf_name = vim.api.nvim_buf_get_name(0)
+  if (vim.tbl_contains(git_filetypes, vim.o.ft) or vim.tbl_contains(git_filetypes, vim.fs.basename(buf_name))) then
     return vim.fs.root(vim.uv.cwd(), ".git")
   else
-    return direnv_opts.buffer_setup.get_cwd()
+    if vim.uv.fs_stat(buf_name) then
+      return vim.fs.dirname(buf_name)
+    else
+      return nil
+    end
   end
 end
-return {"davidmh/direnv.nvim", branch = "custom-setup", opts = {async = true, type = "custom", custom_setup = {get_cwd = get_cwd}}}
+return {"davidmh/direnv.nvim", branch = "custom-setup", opts = {async = true, get_cwd = get_cwd}}
