2fa245ffa42b3931c6509d6aa95391772da414d1
David Mejorado <david.mejorado@gmail.com>
2026-01-03 18:50:37 -0800

    tune in grug-far keymaps

--- nvim/fnl/plugins/find-and-replace.fnl
+++ nvim/fnl/plugins/find-and-replace.fnl
@@ -1,10 +1,19 @@
 (import-macros {: tx : augroup : nmap} :own.macros)
+(local {: autoload} (require :nfnl.module))
+(local grug-far (autoload :grug-far))
 
-(fn open-result-and-close-search []
-  (nmap :<C-enter> :<localleader>o<localleader>c {:buffer true :remap true}))
+(fn open-quickfix-and-close-grug []
+  (local instance (grug-far.get_instance 0))
+  (instance:open_quickfix)
+  (instance:close)
+  (vim.cmd.cfirst))
 
-(augroup :grug-far-keybindings [:FileType {:pattern :grug-far
-                                           :callback open-result-and-close-search}])
+(augroup :grug-far-keybindings
+         [:FileType {:pattern :grug-far
+                     :callback #(nmap :<C-enter>
+                                      open-quickfix-and-close-grug
+                                      {:buffer true})}])
 
 (tx :MagicDuck/grug-far.nvim {:opts {}
-                              :cmd [:GrugFar]})
+                              :cmd [:GrugFar]
+                              :keys [(tx :<localleader>g :<cmd>GrugFar<cr> {:desc :grug-far :mode :n})]})
--- nvim/lua/plugins/find-and-replace.lua
+++ nvim/lua/plugins/find-and-replace.lua
@@ -1,9 +1,18 @@
 -- [nfnl] fnl/plugins/find-and-replace.fnl
-local function open_result_and_close_search()
-  return vim.keymap.set("n", "<C-enter>", "<localleader>o<localleader>c", {buffer = true, remap = true})
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_.autoload
+local grug_far = autoload("grug-far")
+local function open_quickfix_and_close_grug()
+  local instance = grug_far.get_instance(0)
+  instance:open_quickfix()
+  instance:close()
+  return vim.cmd.cfirst()
 end
 do
   local group = vim.api.nvim_create_augroup("grug-far-keybindings", {clear = true})
-  vim.api.nvim_create_autocmd("FileType", {pattern = "grug-far", callback = open_result_and_close_search, group = group})
+  local function _2_()
+    return vim.keymap.set("n", "<C-enter>", open_quickfix_and_close_grug, {buffer = true})
+  end
+  vim.api.nvim_create_autocmd("FileType", {pattern = "grug-far", callback = _2_, group = group})
 end
-return {"MagicDuck/grug-far.nvim", opts = {}, cmd = {"GrugFar"}}
+return {"MagicDuck/grug-far.nvim", opts = {}, cmd = {"GrugFar"}, keys = {{"<localleader>g", "<cmd>GrugFar<cr>", desc = "grug-far", mode = "n"}}}
